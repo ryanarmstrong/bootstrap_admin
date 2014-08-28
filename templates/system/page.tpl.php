2c7cbf3bdb261a8c2d5ebcdf551a8e25df8ad6e7
ryanarmstrong <jenova49@gmail.com>
2014-08-28 16:39:47 -0700

    Updated theme for the latest version of Bootstrap theme.

--- templates/system/page.tpl.php
+++ templates/system/page.tpl.php
@@ -77,23 +77,31 @@
 
 <div class="main-container container-fluid">
 
-  <?php /* region--header.tpl.php */ ?>
-  <?php print render($page['header']); ?>
-
   <div class="row">
 
-    <?php /* region--sidebar.tpl.php */ ?>
-    <?php if ($page['sidebar_first']): ?>
-      <?php print render($page['sidebar_first']); ?>
-    <?php endif; ?>
-
-    <?php /* region--content.tpl.php */ ?>
-    <?php print render($page['content']); ?>
-
-    <?php /* region--sidebar.tpl.php */ ?>
-    <?php if ($page['sidebar_second']): ?>
-      <?php print render($page['sidebar_second']); ?>
-    <?php endif; ?>
+	  <section<?php print $content_column_class; ?>>
+	      <?php if (!empty($page['highlighted'])): ?>
+	        <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
+	      <?php endif; ?>
+	      <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
+	      <a id="main-content"></a>
+	      <?php print render($title_prefix); ?>
+	      <?php if (!empty($title)): ?>
+	        <h1 class="page-header"><?php print $title; ?></h1>
+	      <?php endif; ?>
+	      <?php print render($title_suffix); ?>
+	      <?php print $messages; ?>
+	      <?php if (!empty($tabs) && !_is_node_page()): ?>
+	        <?php print render($tabs); ?>
+	      <?php endif; ?>
+	      <?php if (!empty($page['help'])): ?>
+	        <?php print render($page['help']); ?>
+	      <?php endif; ?>
+	      <?php if (!empty($action_links)): ?>
+	        <ul class="action-links"><?php print render($action_links); ?></ul>
+	      <?php endif; ?>
+	      <?php print render($page['content']); ?>
+	    </section>
 
   </div>
 </div>
